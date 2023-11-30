c2166e0b28b415f3dade628da9a61c145f718536
Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2023-11-30 05:36:50 +0000

    Task 13 solution added

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,23 +1,23 @@
 #include "binary_trees.h"
 /**
- * binary_tree_nodes - function that returns # of nodes in a tree with children
- * @tree: tree to check
- * Return: number of nodes with children
+ * binary_tree_nodes - returns number of nodes in a tree with children
+ * @tree: pointer
+ * Return: size_t nodes with children
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 
-	size_t  node = 0;
+	size_t  number_of_nodes = 0;
 
-	if (tree == NULL)
+	if (!tree)
 	{
 		return (0);
 	}
 	else
 	{
-		node += ((tree->left || tree->right) ? 1 : 0);
-		node += binary_tree_nodes(tree->left);
-		node += binary_tree_nodes(tree->right);
-		return (node);
+		number_of_nodes += ((tree->left || tree->right) ? 1 : 0);
+		number_of_nodes += binary_tree_nodes(tree->left);
+		number_of_nodes += binary_tree_nodes(tree->right);
+		return (number_of_nodes);
 	}
 }
