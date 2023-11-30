48673568de0c4ba403ba0939e3ded48e20cc931a
Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2023-11-30 05:54:18 +0000

    Task 16  solution added

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,29 +1,4 @@
 #include "binary_trees.h"
-
-/**
- * binary_tree_is_perfect - checks if tree is perfect
- * @tree: pointer to tree to check
- * Return: in 1 or 0
- */
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-	int tree_check = 0;
-
-	if (!tree)
-	{
-		return (0);
-	}
-	else
-	{
-		tree_check = do_perfect_tree_check(tree);
-		if (tree_check != 0)
-		{
-			return (1);
-		}
-		return (0);
-	}
-}
-
 /**
  * do_perfect_tree_check - does actual perfect tree check
  * @tree: pointer to tree to check
@@ -47,6 +22,29 @@ int do_perfect_tree_check(const binary_tree_t *tree)
 	}
 	else
 	{
+		return (0);
+	}
+}
+/**
+ * binary_tree_is_perfect - checks if tree is perfect
+ * @tree: pointer to tree to check
+ * Return: in 1 or 0
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int tree_check = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+	else
+	{
+		tree_check = do_perfect_tree_check(tree);
+		if (tree_check != 0)
+		{
+			return (1);
+		}
 		return (0);
 	}
 }
