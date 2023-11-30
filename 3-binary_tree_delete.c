dac71f9350e1f585853f548a8bdf00f40c536634
Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2023-11-30 04:34:58 +0000

    Task 3 solution added

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_delete - deletes a binary tree
+* @tree: binary tree to delete
+* Return: void
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	else
+	{
+		if (!tree)
+		{
+			free(tree);
+		}
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+	}
+}
